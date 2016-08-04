5f2d9bfef397334c7061734c339ab2339254a619
Stephane Delcroix <stephane@delcroix.org>
2016-08-04 11:47:40 +0200

    [iOS] move LayoutExtensions and NAtiveBinding extensions

--- Xamarin.Forms.Platform.iOS/Extensions/UIViewExtensions.cs
+++ Xamarin.Forms.Platform.iOS/Extensions/UIViewExtensions.cs
@@ -28,6 +28,32 @@ namespace Xamarin.Forms.Platform.iOS
 			return new SizeRequest(request, minimum);
 		}
 
+		public static void SetBinding(this UIView view, string propertyName, BindingBase binding, string eventSourceName)
+		{
+			NativeBindingHelpers.SetBinding(view, propertyName, binding, eventSourceName);
+		}
+
+		public static void SetBinding(this UIView view, string propertyName, BindingBase binding)
+		{
+			NativeViewPropertyListener nativePropertyListener = null;
+			if (binding.Mode == BindingMode.TwoWay) {
+				nativePropertyListener = new NativeViewPropertyListener(propertyName);
+				view.AddObserver(nativePropertyListener, propertyName, 0, IntPtr.Zero);
+			}
+
+			NativeBindingHelpers.SetBinding(view, propertyName, binding, nativePropertyListener);
+		}
+
+		public static void SetValue(this UIView target, BindableProperty targetProperty, object value)
+		{
+			NativeBindingHelpers.SetValue(target, targetProperty, value);
+		}
+
+		public static void SetBindingContext(this UIView target, object bindingContext, Func<UIView, IEnumerable<UIView>> getChildren = null)
+		{
+			NativeBindingHelpers.SetBindingContext(target, bindingContext, getChildren);
+		}
+
 		internal static T FindDescendantView<T>(this UIView view) where T : UIView
 		{
 			var queue = new Queue<UIView>();
